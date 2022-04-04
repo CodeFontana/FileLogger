e521b58fe1cb67eec515ad0e0916729180d91e95
Brian Fontana <brian@codefoxtrot.com>
2022-04-04 01:22:15 -0400

    Add remarks and add LogLevelColors property

--- FileLoggerLibrary/FileLoggerOptions.cs
+++ FileLoggerLibrary/FileLoggerOptions.cs
@@ -16,23 +16,47 @@ public class FileLoggerOptions
     /// Determines the log file location.
     /// <remarks>
     /// If unspecified, the default location will be a 'log' folder created
-    /// at the path of the currently exeecuting process.
+    /// at the path of the currently executing process.
     /// </remarks>
     /// </summary>
     public string LogFolder { get; set; } = "";
 
     /// <summary>
     /// Determines the maximum size, in bytes, of an individual log file.
+    /// <remarks>
+    /// The default log file size is 50MB.
+    /// </remarks>
     /// </summary>
     public long LogMaxBytes { get; set; } = 50 * 1048576;
 
     /// <summary>
     /// Determines the maximum number of log files.
+    /// <remarks>
+    /// The default is 10 log files.
+    /// </remarks>
     /// </summary>
     public uint LogMaxCount { get; set; } = 10;
 
     /// <summary>
     /// Determines the minimum logging level to be logged.
+    /// <remarks>
+    /// The default is LogLevel.Trace, for maximum logging.
+    /// </remarks>
     /// </summary>
     public LogLevel LogMinLevel { get; set; } = LogLevel.Trace;
+
+    /// <summary>
+    /// Mapping of Microsoft.Extensions.Logging.LogLevel to different
+    /// console colors.
+    /// </summary>
+    public Dictionary<LogLevel, ConsoleColor> LogLevelColors { get; set; } = new()
+    {
+        [LogLevel.Trace] = ConsoleColor.Cyan,
+        [LogLevel.Debug] = ConsoleColor.Blue,
+        [LogLevel.Information] = ConsoleColor.Green,
+        [LogLevel.Warning] = ConsoleColor.Yellow,
+        [LogLevel.Error] = ConsoleColor.Red,
+        [LogLevel.Critical] = ConsoleColor.DarkRed,
+        [LogLevel.None] = ConsoleColor.White
+    };
 }
