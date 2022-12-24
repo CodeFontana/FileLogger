3896a2d9c96918ff4fba2bdc274e282507718c1f
Brian Fontana <brian@codefoxtrot.com>
2022-12-24 17:37:18 -0500

    Update to .NET 7.0

--- FileLoggerDemo/Program.cs
+++ FileLoggerDemo/Program.cs
@@ -1,5 +1,4 @@
 ﻿using FileLoggerLibrary;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -12,47 +11,11 @@ internal class Program
     {
         try
         {
-            string env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
-            bool isDevelopment = string.IsNullOrEmpty(env) || env.ToLower() == "development";
-
             await Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(config =>
-                {
-                    config.SetBasePath(Directory.GetCurrentDirectory());
-                    config.AddJsonFile("appsettings.json", true, true);
-                    config.AddJsonFile($"appsettings.{env}.json", true, true);
-                    config.AddUserSecrets<Program>(optional: true);
-                    config.AddEnvironmentVariables();
-                })
                 .ConfigureLogging((context, builder) =>
                 {
                     builder.ClearProviders();
-
-                    // Use IConfiguration (e.g. appsettings.json)
                     builder.AddFileLogger(context.Configuration);
-
-                    // Use extension method callback
-                    //logging.AddFileLogger(builder =>
-                    //{
-                    //    configure.LogName = "FileLoggerDemo";
-                    //    configure.LogFolder = $@"{Environment.CurrentDirectory}\log";
-                    //    configure.LogMaxBytes = 50 * 1048576;
-                    //    configure.LogMaxCount = 10;
-                    //    configure.LogMinLevel = LogLevel.Trace;
-                    //    configure.IndentMultilineMessages = true;
-                    //    configure.ConsoleLogging = true;
-                    //    configure.EnableConsoleColors = true;
-                    //    configure.LogLevelColors = new Dictionary<LogLevel, ConsoleColor>()
-                    //    {
-                    //        [LogLevel.Trace] = ConsoleColor.Cyan,
-                    //        [LogLevel.Debug] = ConsoleColor.Blue,
-                    //        [LogLevel.Information] = ConsoleColor.Green,
-                    //        [LogLevel.Warning] = ConsoleColor.Yellow,
-                    //        [LogLevel.Error] = ConsoleColor.Red,
-                    //        [LogLevel.Critical] = ConsoleColor.DarkRed,
-                    //        [LogLevel.None] = ConsoleColor.White
-                    //    };
-                    //});
                 })
                 .ConfigureServices((hostContext, services) =>
                 {
